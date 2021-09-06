5b175874f4707496d556e58bc78e2d10600e91c3
Gessé Carlos <gesse.dev@outlook.com>
2021-09-06 05:44:19 -0300

    routes fixed

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
       <Switch>
         <Route
           exact
-          path="/"
+          path="/rtl-pokedex"
           render={ ({ match }) => this.renderPokedex(match) }
         />
         <Route
@@ -96,7 +96,7 @@ class App extends Component {
       <div className="App">
         <h1>Pokédex</h1>
         <nav>
-          <Link className="link" to="/">{`Home`}</Link>
+          <Link className="link" to="/rtl-pokedex">{`Home`}</Link>
           <Link className="link" to="/about">{`About`}</Link>
           <Link className="link" to="/favorites">{`Favorite Pokémons`}</Link>
         </nav>
